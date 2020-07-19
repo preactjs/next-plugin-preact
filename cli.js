292cd819d72f7b281db5973c7d47e8f3373291dd
Sven Tschui <sventschui@gmail.com>
2020-07-19 11:16:17 +0200

    Upgrade to newer next.js version

--- cli.js
+++ cli.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
-require('./alias')();
-require('next/dist/bin/next');
+const fs = require("fs");
+const path = require("path");
+require("./alias")();
+
+function patchWorkerJs() {
+  const workerJs = require.resolve("next/dist/export/worker.js");
+  const aliasJs = path.join(__dirname, "./alias.js");
+  const aliasPath = path.relative(path.dirname(workerJs), aliasJs);
+  const requireStatement = `require("${aliasPath}")();`;
+
+  const content = fs.readFileSync(workerJs, "utf8");
+
+  if (content.indexOf(requireStatement) === -1) {
+    fs.writeFileSync(workerJs, requireStatement + content);
+  }
+}
+
+patchWorkerJs();
+require("next/dist/bin/next");
